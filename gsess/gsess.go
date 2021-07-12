0a9ac082e62df9dcec288b8349c9173bc84d6cfd
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2021-07-11 20:25:00 -0400

    update-automated-save

--- gsess/gsess.go
+++ gsess/gsess.go
@@ -7,10 +7,12 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/aws/aws-sdk-go/service/sqs"
 
+	"log"
 	"net/http"
 	"strings"
 	"time"
@@ -277,4 +279,47 @@ func MsgTakeApart(messages []*sqs.Message) (string, string, map[string]string) {
 		}
 	}
 	return msgBody, msgStr, m
+}
+
+func (gsa *GSA) CreateInstance() error {
+
+	sess := gsa.Sess
+
+	// Create EC2 service client
+	svc := ec2.New(sess)
+
+	// Specify the details of the instance that you want to create.
+	runResult, err := svc.RunInstances(&ec2.RunInstancesInput{
+		// An Amazon Linux AMI ID for t2.micro instances in the us-west-2 region
+		ImageId:      aws.String("ami-e7527ed7"),
+		InstanceType: aws.String("t2.micro"),
+		MinCount:     aws.Int64(1),
+		MaxCount:     aws.Int64(1),
+	})
+
+	if err != nil {
+		fmt.Println("Could not create instance", err)
+		return err
+	}
+
+	fmt.Println("Created instance", *runResult.Instances[0].InstanceId)
+
+	// Add tags to the created instance
+	_, errtag := svc.CreateTags(&ec2.CreateTagsInput{
+		Resources: []*string{runResult.Instances[0].InstanceId},
+		Tags: []*ec2.Tag{
+			{
+				Key:   aws.String("Name"),
+				Value: aws.String("MyFirstInstance"),
+			},
+		},
+	})
+	if errtag != nil {
+		log.Println("Could not create tags for instance", runResult.Instances[0].InstanceId, errtag)
+		return errtag
+	}
+
+	fmt.Println("Successfully tagged instance")
+
+	return nil
 }
